d6de126297c33fcfe3a64fbffe3bc0845480410a
zhangzengyang <zhangzengyang@qq.com>
2017-06-21 11:10:21 +0800

    添加ajax请求

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-$(function(){
-	$('.btn-search').click(function(){
+$(function() {
+	$('.btn-search').click(function() {
 		$('.table-search').removeClass('hide');
+		load();
 	});
+
+	function load() {
+		$.ajax({
+			type: "get",
+			url: "http://59.37.77.114:8080/api/",
+			async: true
+		}).done(function(response) {
+			console.log(response);
+		});
+	};
 });
