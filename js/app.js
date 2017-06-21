ca52a7989e9f71664518c7236bb7900a1a3b6414
zongyang <zhangzengyang@qq.com>
2017-06-21 22:37:43 +0800

    添加绝对地址

--- js/app.js
+++ js/app.js
@@ -27,75 +27,30 @@
 	});
 	app.service('model', function($http) {
 		this.getUser = function(keyword, cb) {
-			/*cb({
-				"id": 'wqe',
-				"name": "haibor",
-				"state": "1",
-				"realname": "田海波",
-				"password": "111111",
-				"email": "haibor@qq.com",
-				"phone": "18926265656",
-				"region": "天河",
-				"power": "1",
-				"admin": "1",
-				"other1": null,
-				"other2": null,
-				"_links": {
-					"self": {
-						"href": "http://59.37.77.114:8080/api/users/1"
-					},
-					"user": {
-						"href": "http://59.37.77.114:8080/api/users/1"
-					},
-					"datis": {
-						"href": "http://59.37.77.114:8080/api/users/1/datis"
-					}
-				}
-			})
-			return;*/
 			var params = {
-				keyword: keyword
+				name: keyword
 			};
 			$http({
 				type: 'GET',
 				params: params,
-				url: '/api/users/search/findByNameAllIgnoringCase',
+				url: 'http://59.37.77.114:8080/api/users/search/findByNameAllIgnoringCase',
 			}).then(function(response) {
 				cb(response.data);
 
 			});
 		};
 		this.getScore = function(id, cb) {
-
-			/*cb([{
-				"user_answer": "A",
-				"number": 1,
-				"leibie": "one",
-				"wenjuan_id": 1,
-				"tigan": "你的学校是？",
-				"right_answers": "A",
-				"defen": 10
-			}, {
-				"user_answer": "A|B",
-				"number": 2,
-				"leibie": "many",
-				"wenjuan_id": 1,
-				"tigan": "你的单位是？",
-				"right_answers": "C|D|E|F",
-				"defen": 0
-			}])
-			return;*/
 			var params = {
 				user_id: id
 			};
 			$http({
 				type: 'GET',
 				params: params,
-				url: '/api/userti',
+				url: 'http://59.37.77.114:8080/userti',
 			}).then(function(response) {
 				cb(response.data);
 			});
 		};
 	});
-
 })()
+
